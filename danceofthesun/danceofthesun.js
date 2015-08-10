c26471baf364a668a37c99e9c2e508da9d7b1b3b
Stuart <gravatar@strudel.org.uk>
2015-08-11 00:58:52 +0100

    Movable Sun

--- danceofthesun/danceofthesun.js
+++ danceofthesun/danceofthesun.js
@@ -3,6 +3,7 @@ function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
 r(function(){
 
 	var positions;
+	var w,h,scale;
 	
 	function parseFile(d,attrs){
 
@@ -24,27 +25,46 @@ r(function(){
 
 	// Load the files
 	loadJSON('data/barycentre.json',parseFile,{});
-
+	var sun = new Array();
 
 	function drawIt(){
 	
 		var el = $('#holder');
 	
 		// Calculate values
-		var w,h;
 		h = $(window).height()-$('#header').outerHeight();
 		w = h;
-		el.html('').css({'width':w+'px','height':h+'px'});
+		el.html('').css({'width':w+'px','height':h+'px','margin':'auto'});
 		var solarsystem = Raphael("holder", w, h);
 		$('#holder svg').attr({'id':'canvas'});
 		var path = "M 0,0 ";
-		var colour = '#fdcd00';
 		var x,y;
-		var scale = w/3e6;
+		scale = w/4.5e6;
 		var prevdate = new Date('1970-01-01T00:00:00Z');
 		var j = 0;
 		var now = new Date();
+		var nowjd = getJD(now);
 		var atyear = now.getFullYear();
+		var foundnow = false;
+	
+		$("#slider").slider({
+			min: 1978,
+			max: 2042,
+			value: now.getFullYear(),
+			create: function(event, ui){
+				updateData(now.getFullYear())
+			},
+			slide: function(event, ui){
+				updateData(ui.value)
+			}
+		});
+
+		// Draw Sun
+		sun.push(solarsystem.circle(0,0,696342*scale).attr({'fill':'#ffde00','fill-opacity':1,'stroke':0}));	
+		// Draw radiative zone
+		sun.push(solarsystem.circle(0,0,0.7*696342*scale).attr({'fill':'#fff79a','fill-opacity':0.3,'stroke':0}));	
+		// Draw core
+		sun.push(solarsystem.circle(0,0,0.25*696342*scale).attr({'fill':'#fffcd5','fill-opacity':0.3,'stroke':0}));	
 
 		
 		// Draw space
@@ -53,28 +73,74 @@ r(function(){
 			y = (h/2) - scale*positions[i].r*Math.sin(Math.PI*positions[i].l/180);
 			path += ' '+(j > 0 ? 'L':'M')+x+','+y;
 			// The Julian Date of the Unix Time epoch is 2440587.5
-			var d = new Date((positions[i].jd-2440587.5)*86400000);
+			var d = getDateFromJD(positions[i].jd);
 			var year = d.getFullYear();
 			if(year > prevdate.getFullYear()){
-				solarsystem.circle(x,y,2).attr({'fill':'#000000','stroke':0});	
+				solarsystem.circle(x,y,1.5).attr({'fill':'#f6881f','stroke':0});	
 				if(year % 2 == 0) solarsystem.text(x+7,y,''+year).attr({'stroke':0,'fill':'#000000','text-anchor':'start'});
-				if(year == atyear){
-					// Draw Sun
-					solarsystem.circle(x,y,696342*scale).attr({'fill':'#feee00','fill-opacity':1,'stroke':0});	
-					// Draw radiative zone
-					solarsystem.circle(x,y,0.7*696342*scale).attr({'fill':'#feee00','fill-opacity':0.3,'stroke':0});	
-					// Draw core
-					solarsystem.circle(x,y,0.25*696342*scale).attr({'fill':'#ffffff','fill-opacity':0.3,'stroke':0});	
-				}
 				prevdate = d;
+			}
+			if(!foundnow && Math.abs(nowjd-positions[i].jd) < 3){
+				foundnow = true;
+				moveSun(x,y);
 			}
 		}
 	
 		// Draw barycentre path
-		solarsystem.path(path).attr({'stroke':colour,'stroke-width':1,'opacity':1});
+		solarsystem.path(path).attr({'stroke':'#f6881f','stroke-width':0.5,'opacity':1});
 	
 		solarsystem.path('M'+(w/2)+','+(h/2)+' l-5,0 l10,0 l-5,0 l0,5 l0,-10 z').attr({'stroke':'black'})
 	
 		$('.loader').remove();
+	}
+	
+	function getDateFromJD(jd){
+		// The Julian Date of the Unix Time epoch is 2440587.5
+		return new Date((jd-2440587.5)*86400000);
+	}
+	function moveSun(x,y){
+		for(var i = 0 ; i < sun.length;i++) sun[i].transform('T'+x+','+y);
+	}
+	function getIndexFromYear(year){
+		var i,d,yr,x,y;
+		for(i = 0; i < positions.length; i++){
+			d = getDateFromJD(positions[i].jd);
+			if(d.getFullYear()==year) return i;
+		}
+		return positions.length-1;
+	}
+	function updateData(year){
+
+		var d,yr,x,y;
+		var i = getIndexFromYear(year);
+		
+		d = new Date((positions[i].jd-2440587.5)*86400000);
+		x = (w/2) - scale*positions[i].r*Math.cos(Math.PI*positions[i].l/180);
+		y = (h/2) - scale*positions[i].r*Math.sin(Math.PI*positions[i].l/180);
+		moveSun(x,y);
+
+		/*$(".titleyear").html(" in "+year);
+		var i;
+		for(i = data.length-1 ; i > 0 ; i--){
+			if(year >= data[i].year) break;
+		}
+		var output = 'In '+year+' there were<div class="count">'+data[i].planets.count+'<\/div>';
+		//if(data[i].dwarf.count > 0) output += ' and <div class="count">'+data[i].dwarf.count+'<\/div> dwarf planets';
+		$("#output").html(output)
+		
+		if(data[i].description) $("#info").html(data[i].description)
+		
+		var planets = "";
+		for(var p = 0; p < data[i].planets.names.length; p++){
+			planets += '<div class="planet '+data[i].planets.names[p]+'"><div class="disc"><\/div><div class="label">'+data[i].planets.names[p]+'<\/div><\/div>';
+		}
+		$("#planets").html(planets)*/
+	
+	}
+	
+	function getJD(clock) {
+		// The Julian Date of the Unix Time epoch is 2440587.5
+		if(!clock) clock = this.clock;
+		return ( clock.getTime() / 86400000.0 ) + 2440587.5;
 	}
 });
