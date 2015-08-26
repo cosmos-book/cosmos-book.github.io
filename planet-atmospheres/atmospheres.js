697d120e11e5dde359a0f46c10707f788d09e7d3
Stuart <gravatar@strudel.org.uk>
2015-08-26 21:08:11 +0100

    Add pressure profiles as opacity layers

--- planet-atmospheres/atmospheres.js
+++ planet-atmospheres/atmospheres.js
@@ -15,20 +15,30 @@ r(function(){
 	$('#holder svg').attr('id','canvas');
 	var svg = paper.set();
 	var loaded = 0;
-	var atmospheres = [];
+	var atmospheres = {};
 
 	function parseFile(d,attrs){
-		atmospheres.push({'name':attrs.planet,'data':CSV2JSON(d,[
-			{'name':'lower','format':'number'},
-			{'name':'upper','format':'number'},
-			{'name':'feature','format':'string'},
-			{'name':'name','format':'string'},
-			{'name':'T','format':'number'},
-			{'name':'P','format':'number'},
-			{'name':'colour','format':'string'}
-		])});
+		if(!atmospheres[attrs.planet]) atmospheres[attrs.planet] = {};
+		if(attrs.profile){
+			//Altitude (km),Pressure (mbar),Temperature (K)
+			atmospheres[attrs.planet].profile = CSV2JSON(d,[
+				{'name':'h','format':'number'},
+				{'name':'P','format':'number'},
+				{'name':'T','format':'number'}
+			]);
+		}else{
+			atmospheres[attrs.planet].data = CSV2JSON(d,[
+				{'name':'lower','format':'number'},
+				{'name':'upper','format':'number'},
+				{'name':'feature','format':'string'},
+				{'name':'name','format':'string'},
+				{'name':'T','format':'number'},
+				{'name':'P','format':'number'},
+				{'name':'colour','format':'string'}
+			]);
+		}
 		loaded++;
-		if(loaded==planets.length) drawIt();
+		if(loaded==planets.length*2) drawIt();
 	}
 
 	$('.loader').remove();
@@ -36,6 +46,7 @@ r(function(){
 
 	// Load the files
 	for(var p = 0; p < planets.length; p++) loadCSV('data/'+planets[p].toLowerCase()+'.csv',parseFile,{'planet':planets[p]});
+	for(var p = 0; p < planets.length; p++) loadCSV('data/'+planets[p].toLowerCase()+'_profile.csv',parseFile,{'planet':planets[p],'profile':true});
 
 	function getColour(n){
 
@@ -51,30 +62,75 @@ r(function(){
 	}
 	
 	
+
+	function buildCSSGradient(stops){
+		// background: #1e5799; /* Old browsers */
+		// background: -moz-linear-gradient(top,  #1e5799 0%, #2989d8 50%, #207cca 51%, #7db9e8 100%); /* FF3.6+ */
+		// background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#1e5799), color-stop(50%,#2989d8), color-stop(51%,#207cca), color-stop(100%,#7db9e8)); /* Chrome,Safari4+ */
+		// background: -webkit-linear-gradient(top,  #1e5799 0%,#2989d8 50%,#207cca 51%,#7db9e8 100%); /* Chrome10+,Safari5.1+ */
+		// background: -o-linear-gradient(top,  #1e5799 0%,#2989d8 50%,#207cca 51%,#7db9e8 100%); /* Opera 11.10+ */
+		// background: -ms-linear-gradient(top,  #1e5799 0%,#2989d8 50%,#207cca 51%,#7db9e8 100%); /* IE10+ */
+		// background: linear-gradient(to bottom,  #1e5799 0%,#2989d8 50%,#207cca 51%,#7db9e8 100%); /* W3C */
+		// filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#1e5799', endColorstr='#7db9e8',GradientType=0 ); /* IE6-9 */
+
+		var s = "";
+		var c = "";
+		var css = "";
+		for(var i = 0; i < stops.length;i++){
+			if(s != "") s += ',';
+			s += ''+stops[i][0]+' '+stops[i][1]+'%';
+			if(c != "") c += ',';
+			c += 'color-stop('+stops[i][1]+'%,'+stops[i][0]+')';
+		}
+		css = 'background: '+stops[0][0]+';'; // Old browsers
+		css += 'background: -moz-linear-gradient(top, '+s+');';	// FF3.6+
+		css += 'background: -webkit-gradient(linear, left top, left bottom, '+c+');';	// Chrome,Safari4+
+		css += 'background: -webkit-linear-gradient(top, '+s+');';	 // Chrome10+,Safari5.1+
+		css += 'background: -o-linear-gradient(top, '+s+');';	 // Opera 11.10+
+		css += 'background: -ms-linear-gradient(top, '+s+');';	 // IE10+
+		css += 'background: linear-gradient(to bottom, '+s+');';	 // W3C
+		css += 'filter: progid:DXImageTransform.Microsoft.gradient( startColorstr=\''+stops[0][0]+'\', endColorstr=\''+stops[stops.length-1][0]+'\',GradientType=0 );'; // IE6-9
+		return css;		
+	}
+	
 	// Draw the result
 	function drawIt(){
-
-		dx = w/(atmospheres.length*2);
+	
+		dx = w/(planets.length*2);
 		dy = h/(range.y[1]-range.y[0]);
 
-		//console.log(atmospheres,w,dx,atmospheres.length,dy)
+		var j = 0;
+		for(var i in atmospheres){
 
-		for(var i = 0; i < atmospheres.length; i++){
-			//console.log(atmospheres[i].data,atmospheres[i].data.length)
 			if(atmospheres[i]){
-				x = (dx*2*(i)+dx*0.5);
+
+				x = (dx*2*j+dx*0.5);
 
 				// Make graded background
-				grad = "90-rgb(255,255,255):0-rgb(255,255,255):75-#e1f4fe";
-				paper.rect(x,0,dx,h/2).attr({'fill':grad,'stroke':0});
+				grad = '';
 				hassurface = true;
 				for(var d = 0; d < atmospheres[i].data.length; d++){
 					if(atmospheres[i].data[d].lower < 0) hassurface = false;
 				}
-				if(!hassurface){
-					grad = "90-#e1f4fe:0-rgb(255,255,255):25-rgb(255,255,255)";
-					paper.rect(x,h/2,dx,h/2).attr({'fill':grad,'stroke':0});
+				if(atmospheres[i].profile){
+					stops = [['rgba(255,255,255,0)',0]];
+					for(var k=atmospheres[i].profile.length-1; k >= 0; k--){
+						if(atmospheres[i].profile[k].h < range.y[1]){
+							op = (atmospheres[i].profile[k].P < 1000 ? (Math.log10(atmospheres[i].profile[k].P)+6)/9 : 1);
+							if(op < 0) op = 0;
+							y = 100*((range.y[1]-atmospheres[i].profile[k].h)/(range.y[1]-range.y[0]));
+							stops.push(['rgba(255,255,255,'+op.toFixed(2)+')',y.toFixed(2)]);
+						}
+					}
+					if(hassurface) stops.push(['rgba(255,255,255,0)',50.001]);
+					else stops.push(['rgba(255,255,255,0)',100]);
+					grad = buildCSSGradient(stops);
+				}else{
+					if(hassurface) grad = buildCSSGradient([['rgba(255,255,255,0)',0],['rgba(255,255,255,1)',20],['rgba(255,255,255,1)',50],['rgba(255,255,255,0)',50.001]]);
+					else grad = buildCSSGradient([['rgba(255,255,255,0)',0],['rgba(255,255,255,1)',20],['rgba(255,255,255,1)',80],['rgba(255,255,255,0)',100]]);
 				}
+				$('#holder').append('<div style="'+grad+';width: '+dx+'px;height:'+h+'px;position: absolute; left:'+x+'px;top:0px;z-index:0;"></div>')
+
 
 				for(var d = 0; d < atmospheres[i].data.length; d++){
 					y = h-(atmospheres[i].data[d].upper-range.y[0])*dy;
@@ -85,218 +141,24 @@ r(function(){
 					p = dx*0.05;
 					c = getColour(atmospheres[i].data[d].name);
 					if(atmospheres[i].data[d].feature.toLowerCase().indexOf('cloud layer') >= 0){
-						console.log(atmospheres[i].name,'layer',atmospheres[i].data[d].upper,atmospheres[i].data[d].lower,atmospheres[i].data[d].feature)
+						//console.log(i,'layer',atmospheres[i].data[d].upper,atmospheres[i].data[d].lower,atmospheres[i].data[d].feature)
 						paper.rect(x,y,dx,y2).attr({'fill':c,'stroke':0,'opacity':0.5,'title':atmospheres[i].data[d].name})
 					}
 					if(atmospheres[i].data[d].feature.indexOf('Boundary') == 0){
-						console.log(atmospheres[i].name,'boundary',atmospheres[i].data[d].upper,atmospheres[i].data[d].lower,atmospheres[i].data[d].feature)
+						//console.log(i,atmospheres[i],'boundary',atmospheres[i].data[d].upper,atmospheres[i].data[d].lower,atmospheres[i].data[d].feature)
 						paper.path('M'+(x-p)+','+y+'l'+(dx+2*p)+',0').attr({'stroke':'black','stroke-width':0.5,'stroke-dasharray':'- '})
 						paper.text(x-p,y,atmospheres[i].data[d].name).attr({'text-anchor':'end','fill':'black','stroke':0})
 					}
-					
 				}
-				paper.text(x+dx*0.5,h-10,atmospheres[i].name).attr({'text-anchor':'middle','fill':'black','stroke':0})
+				paper.text(x+dx*0.5,h-10,i).attr({'text-anchor':'middle','fill':'black','stroke':0})
 			}
+			j++;
 		}
 
 
-		return;
-		var minx = 0;
-		var maxx = 366;
-		var miny = range.y[0];
-		var maxy = range.y[1];
-		var months = [31,28,31,30,31,30,31,31,30,31,30,31];
-		var monthnames = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-
-		for(var i = 0; i < lunar.length; i++){
-			lunar[i].d = new Date(lunar[i].date);
-			if(lunar[i].jd < 2086404.89002) lunar[i].d.setFullYear(parseInt(lunar[i].date.substr(0,5)))
-			lunar[i].y = (lunar[i].d ? lunar[i].d.getFullYear() : 0);
-		}
-
-		for(var i = 0; i < solar.length; i++){
-			solar[i].d = new Date(solar[i].date)
-			if(solar[i].jd < 2086404.89002) solar[i].d.setFullYear(parseInt(solar[i].date.substr(0,5)))
-			solar[i].y = (solar[i].d ? solar[i].d.getFullYear() : 0);
-		}
-	
-		// Work out size of a day
-		dx = parseFloat(((w-padd.left-padd.right)/(maxx+1-range.x[0])).toFixed(1));
-		//dy = parseFloat(((h-padd.top-padd.bottom)/(maxy+1-miny)).toFixed(1));
-
-		// Draw Moon phases
-		for(var y = miny; y < maxy; y++){
-			leap = (isLeapYear(y) ? 0 : 1);
-			var phases = [];
-			for(var x = range.x[0]+leap; x < maxx; x++){
-				phases.push({'p':moonPhase(dateFromDay(y,x)),'pc':(100*x/(maxx-range.x[0]+leap))});
-			}
-			var stops = [];
-			stops.push({'phase':phases[0].p.toFixed(2),'pc':0});
-			for(var i = 1; i < phases.length-1; i++){
-				if(phases[i].p < phases[i-1].p && phases[i].p < phases[i+1].p) stops.push({'phase':0,'pc':phases[i].pc.toFixed(2)})
-				if(phases[i].p > phases[i-1].p && phases[i].p > phases[i+1].p) stops.push({'phase':1,'pc':phases[i].pc.toFixed(2)})
-			}
-			stops.push({'phase':phases[phases.length-1].p.toFixed(2),'pc':100});
-			var grad = "0";
-			for(var i = 0; i < stops.length; i++){
-				grad += "-rgb("+(stops[i].phase < 0.5 ? 225 : 255)+", "+(stops[i].phase < 0.5 ? 244 : 255)+", "+(stops[i].phase < 0.5 ? 254 : 255)+"):"+stops[i].pc;
-			}
-			paper.rect(padd.left+(leap*dx),getY(y,miny,maxy),(w-padd.left-padd.right-leap*dx),dy).attr({'fill':grad,'stroke':0});
-		}
-
-		// Draw key
-		var s = 5;
-		var y = 20;
-		var xoff = w-padd.right-140;
-		var yoff = padd.top + s;
-		var canvi = new Array();
-		$('#holder').prepend('<ul class="key" id="key"></ul>');
-		// Extract the appropriate keys
-		for(var i = 0; i < key.length; i++){
-			if(key[i].code[0].indexOf('7.2')==0){
-				// Append divs to hold key item
-				$('#key').append('<li class="keyitem"><span id="'+key[i].code[0]+'" class="keysymbol"></span><span class="keylabel">'+key[i].label+'</span></li>');
-				canvi.push(Raphael(key[i].code[0],s*2,s*2));
-				getObjectPath(key[i].code[0],s*2,s*2,5,canvi[canvi.length-1],{'title':key[i].label});
-			}
-		}
-
-		// Draw month boundaries
-		for(var m = 0; m <= months.length; m++){
-			path = '';
-			for(var y = miny; y < maxy; y++){
-				months[1] = (isLeapYear(y) ? 29 : 28);
-				days = (isLeapYear(y) ? 0 : 1);
-				for(var m2 = 0; m2 < m; m2++) days += months[m2];
-
-				path += (!path) ? 'M':'L'
-				// For best anti-aliasing we want the x-coordinate to be shifted 0.5 pixels
-				path += (Math.round(getX(days)-0.5)+0.5)+','+getY(y)+'l0,'+dy;
-			}
-			paper.path(path).attr({'stroke':colours.blue[0],'stroke-width':0.5})
-		}
-
-		// Loop over each Lunar eclipse adding it to the chart
-		for(var i = 0; i < lunar.length; i++){
-			if(lunar[i].y >= miny && lunar[i].y < maxy){
-				avm = "";
-				if(lunar[i]['type'][0]=='T') avm = "7.2.2.1";
-				if(lunar[i]['type'][0]=='P') avm = "7.2.2.2";
-				if(lunar[i]['type'][0]=='N') avm = "7.2.2.3";
-				if(avm){
-					d = new Date(lunar[i].date);
-					attr = {'title':d.toDateString()};
-					if(lunar[i].y >= 1996) attr.cursor = 'pointer';
-					p = drawObject(avm,getX(lunar[i].d.getDOY())+(dx/2)+(isLeapYear(lunar[i].y) ? 0 : dx),getY(lunar[i].y)+dy/2,dy*0.4,attr);
-					// Add event to object
-					var j = i;
-					p.obj.data('i',i).click(function(e){ goTo('lunar',this.data("i")); });
-				}
-			}
-		}
-		// Loop over each Solar eclipse adding it to the chart
-		for(var i = 0; i < solar.length; i++){
-			if(solar[i].y >= miny && solar[i].y < maxy){
-				avm = "";
-				if(solar[i]['type'][0]=='T') avm = "7.2.1.1";
-				if(solar[i]['type'][0]=='P') avm = "7.2.1.2";
-				if(solar[i]['type'][0]=='A') avm = "7.2.1.3";
-				if(solar[i]['type'][0]=='H') avm = "7.2.1.4";
-				if(avm){
-					var j = i;
-					d = new Date(solar[i].date);
-					attr = {'title':d.toDateString()};
-					if(solar[i].y >= 1996) attr.cursor = 'pointer';
-					p = drawObject(avm,getX(solar[i].d.getDOY())+(dx/2)+(isLeapYear(solar[i].y) ? 0 : dx),getY(solar[i].y)+dy/2,dy*0.4,attr);
-					// Add event to object
-					p.obj.data('i',i).click(function(e){ goTo('solar',this.data("i")); });
-				}
-			}
-		}
-
-		// Add year labels
-		for(var y = miny; y < maxy; y++){
-			if(y%4==0){
-				paper.text(padd.left/2,parseFloat(getY(y))+dy/2,y).attr({'fill':'black','stroke':0,'font-size':dy+'px'})
-			}
-		}
-
-		// Add month labels
-		var day = 1;		
-		for(var m = 0; m < monthnames.length; m++){
-			day += months[m]/2;
-			x = parseFloat(getX(day));
-			paper.text(x,padd.top+dy,$(window).width() < 700 ? monthnames[m].substr(0,3) : monthnames[m]).attr({'fill':'black','stroke':0,'text-anchor':'middle','font-size':dy+'px'})
-			day += months[m]/2;
-		}
 		$('.loader').remove();
 	}
 
-	function dateFromDay(year, day){
-		var date = new Date(year, 0); // initialize a date in `year-01-01`
-		return new Date(date.setDate(day)); // add the number of days
-	}
-
-	function getX(x){
-		mn = range.x[0];
-		mx = range.x[1];
-		return parseFloat((padd.left + (w-padd.left-padd.right)*(x-mn)/(mx-mn)).toFixed(1))
-	}
-
-	function getY(y){
-		return parseFloat((padd.top + dy*2 + dy*(y-range.y[0])).toFixed(1))
-	}
-
-
-	function moonPhase(moondate){
-
-		var Epoch = 2447891.5;
- 
- 		var JDnow = getJD(moondate);
-
-		// here is where the previous calculations start
-		// note the new way of calculating D -- the answer is the same
-		D = JDnow - Epoch;                      // find diff from 31 Dec 1989
-		var n = D * (360 / 365.242191);                         //no 46-3
-		if(n > 0) n = n - Math.floor(Math.abs(n / 360)) * 360;    //no 46-3
-		else n = n + (360 + Math.floor(Math.abs(n / 360)) * 360);  //no 46-3
-
-		var Mo = n + 279.403303 - 282.768422;                   //no 46-4;
-		if(Mo < 0) { Mo = Mo + 360 }                            //no 46-4
-		var Ec = 360 * .016713 * Math.sin(Mo * 3.141592654 / 180) / 3.141592654;        //no 46-5
-		var lamda = n + Ec + 279.403303;                        //no 46-6
-		if(lamda > 360) { lamda = lamda - 360 }                 //no 46-6
-		var l = 13.1763966 * D + 318.351648;                    //no 65-4
-		if (l > 0) {
-			l = l - Math.floor(Math.abs(l / 360)) * 360;  //no 65-4
-		} else {
-			l = l + (360 + Math.floor(Math.abs(l / 360)) * 360);  //no 65-4
-		}
-		var Mm = l - .1114041 * D - 36.34041;                   //no 65-5
-		if (Mm > 0) {
-			Mm = Mm - Math.floor(Math.abs(Mm / 360)) * 360;                       //no 65-5
-		} else {
-			Mm = Mm + (360 + Math.floor(Math.abs(Mm / 360)) * 360);                       //no 65-5
-		}
-		var N65 = 318.510107 - .0529539 * D;                    //no 65-6
-		if (N65 > 0) {
-			N65 = N65 - Math.floor(Math.abs(N65 / 360)) * 360;                    //no 65-6
-		} else {
-			N65 = N65 + (360 + Math.floor(Math.abs(N65 / 360)) * 360);                    //no 65-6
-		}
-		var Ev = 1.2739 * Math.sin((2 * (l - lamda) - Mm) * 3.141592654 / 180);         //no 65-7
-		var Ae = .1858 * Math.sin(Mo * 3.141592654 / 180);      //no 65-8
-		var A3 = .37 * Math.sin(Mo * 3.141592654 / 180);        //no 65-8
-		var Mmp = Mm + Ev - Ae - A3;                            //no 65-9
-		var Ec = 6.2886 * Math.sin(Mmp * 3.141592654 / 180);    //no 65-10
-		var A4 = .214 * Math.sin((2 * Mmp) * 3.141592654 / 180);                        //no 65-11
-		var lp = l + Ev + Ec - Ae + A4;                         //no 65-12
-		var V = .6583 * Math.sin((2 * (lp - lamda)) * 3.141592654 / 180);               //no 65-13
-		var lpp = lp + V;                                       //no 65-14
-		var D67 = lpp - lamda;                                  //no 67-2
-		return .5 * (1 - Math.cos(D67 * 3.141592654 / 180));      //no 67-3
-	}
 
 });
 
