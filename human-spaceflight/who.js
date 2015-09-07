a88f55b3abc10a48c8cf8d26d735a94f83ed20bd
Stuart <gravatar@strudel.org.uk>
2015-09-07 03:38:41 +0100

    Add tooltips

--- human-spaceflight/who.js
+++ human-spaceflight/who.js
@@ -10,13 +10,11 @@ $(document).ready(function(){
 	var scrollTop = 0;
 	$('#timelinedummy').css({'min-height':h});
 	$('.loader').remove();
+	var cc = {'ABW':'Aruba','AFG':'Afghanistan','AGO':'Angola','AIA':'Anguilla','ALA':'&Aring;land Islands','ALB':'Albania','AND':'Andorra','ARE':'United Arab Emirates','ARG':'Argentina','ARM':'Armenia','ASM':'American Samoa','ATA':'Antarctica','ATF':'French Southern Territories','ATG':'Antigua and Barbuda','AUS':'Australia','AUT':'Austria','AZE':'Azerbaijan','BDI':'Burundi','BEL':'Belgium','BEN':'Benin','BES':'Bonaire, Sint Eustatius and Saba','BFA':'Burkina Faso','BGD':'Bangladesh','BGR':'Bulgaria','BHR':'Bahrain','BHS':'Bahamas','BIH':'Bosnia and Herzegovina','BLM':'Saint Barthélemy','BLR':'Belarus','BLZ':'Belize','BMU':'Bermuda','BOL':'Bolivia, Plurinational State of','BRA':'Brazil','BRB':'Barbados','BRN':'Brunei Darussalam','BTN':'Bhutan','BVT':'Bouvet Island','BWA':'Botswana','CAF':'Central African Republic','CAN':'Canada','CCK':'Cocos (Keeling) Islands','CHE':'Switzerland','CHL':'Chile','CHN':'China','CIV':'Côte d\'Ivoire','CMR':'Cameroon','COD':'Congo, the Democratic Republic of the','COG':'Congo','COK':'Cook Islands','COL':'Colombia','COM':'Comoros','CPV':'Cabo Verde','CRI':'Costa Rica','CUB':'Cuba','CUW':'Curaçao','CXR':'Christmas Island','CYM':'Cayman Islands','CYP':'Cyprus','CZE':'Czech Republic','DEU':'Germany','DJI':'Djibouti','DMA':'Dominica','DNK':'Denmark','DOM':'Dominican Republic','DZA':'Algeria','ECU':'Ecuador','EGY':'Egypt','ERI':'Eritrea','ESH':'Western Sahara','ESP':'Spain','EST':'Estonia','ETH':'Ethiopia','FIN':'Finland','FJI':'Fiji','FLK':'Falkland Islands (Malvinas)','FRA':'France','FRO':'Faroe Islands','FSM':'Micronesia, Federated States of','GAB':'Gabon','GBR':'United Kingdom','GEO':'Georgia','GGY':'Guernsey','GHA':'Ghana',	'GIB':'Gibraltar','GIN':'Guinea','GLP':'Guadeloupe','GMB':'Gambia','GNB':'Guinea-Bissau','GNQ':'Equatorial Guinea','GRC':'Greece','GRD':'Grenada','GRL':'Greenland','GTM':'Guatemala','GUF':'French Guiana','GUM':'Guam','GUY':'Guyana','HKG':'Hong Kong','HMD':'Heard Island and McDonald Islands','HND':'Honduras','HRV':'Croatia','HTI':'Haiti','HUN':'Hungary','IDN':'Indonesia','IMN':'Isle of Man','IND':'India','IOT':'British Indian Ocean Territory','IRL':'Ireland','IRN':'Iran, Islamic Republic of','IRQ':'Iraq','ISL':'Iceland','ISR':'Israel','ITA':'Italy','JAM':'Jamaica','JEY':'Jersey','JOR':'Jordan','JPN':'Japan','KAZ':'Kazakhstan','KEN':'Kenya','KGZ':'Kyrgyzstan','KHM':'Cambodia','KIR':'Kiribati','KNA':'Saint Kitts and Nevis','KOR':'Korea, Republic of','KWT':'Kuwait','LAO':'Lao People\'s Democratic Republic','LBN':'Lebanon','LBR':'Liberia','LBY':'Libya','LCA':'Saint Lucia','LIE':'Liechtenstein','LKA':'Sri Lanka','LSO':'Lesotho','LTU':'Lithuania','LUX':'Luxembourg','LVA':'Latvia','MAC':'Macao','MAF':'Saint Martin (French part)','MAR':'Morocco','MCO':'Monaco','MDA':'Moldova, Republic of','MDG':'Madagascar','MDV':'Maldives','MEX':'Mexico','MHL':'Marshall Islands','MKD':'Macedonia, the former Yugoslav Republic of','MLI':'Mali','MLT':'Malta','MMR':'Myanmar','MNE':'Montenegro','MNG':'Mongolia','MNP':'Northern Mariana Islands','MOZ':'Mozambique','MRT':'Mauritania','MSR':'Montserrat','MTQ':'Martinique','MUS':'Mauritius','MWI':'Malawi','MYS':'Malaysia','MYT':'Mayotte','NAM':'Namibia','NCL':'New Caledonia','NER':'Niger','NFK':'Norfolk Island','NGA':'Nigeria','NIC':'Nicaragua','NIU':'Niue',	'NLD':'Netherlands','NOR':'Norway','NPL':'Nepal','NRU':'Nauru','NZL':'New Zealand','OMN':'Oman','PAK':'Pakistan','PAN':'Panama','PCN':'Pitcairn','PER':'Peru','PHL':'Philippines','PLW':'Palau','PNG':'Papua New Guinea','POL':'Poland','PRI':'Puerto Rico','PRK':'Korea, Democratic People\'s Republic of','PRT':'Portugal','PRY':'Paraguay','PSE':'Palestine, State of','PYF':'French Polynesia','QAT':'Qatar','REU':'Réunion','ROU':'Romania','RUS':'Russian Federation','RWA':'Rwanda','SAU':'Saudi Arabia','SDN':'Sudan','SEN':'Senegal','SGP':'Singapore','SGS':'South Georgia and the South Sandwich Islands','SHN':'Saint Helena, Ascension and Tristan da Cunha','SJM':'Svalbard and Jan Mayen','SLB':'Solomon Islands','SLE':'Sierra Leone','SLV':'El Salvador','SMR':'San Marino','SOM':'Somalia','SPM':'Saint Pierre and Miquelon','SRB':'Serbia','SSD':'South Sudan','STP':'Sao Tome and Principe','SUR':'Suriname','SVK':'Slovakia','SVN':'Slovenia','SWE':'Sweden','SWZ':'Swaziland','SXM':'Sint Maarten (Dutch part)','SYC':'Seychelles','SYR':'Syrian Arab Republic','TCA':'Turks and Caicos Islands','TCD':'Chad','TGO':'Togo','THA':'Thailand','TJK':'Tajikistan','TKL':'Tokelau','TKM':'Turkmenistan','TLS':'Timor-Leste','TON':'Tonga','TTO':'Trinidad and Tobago','TUN':'Tunisia','TUR':'Turkey','TUV':'Tuvalu','TWN':'Taiwan, Province of China','TZA':'Tanzania, United Republic of','UGA':'Uganda','UKR':'Ukraine','UMI':'United States Minor Outlying Islands','URY':'Uruguay','USA':'United States','UZB':'Uzbekistan','VAT':'Holy See (Vatican City State)','VCT':'Saint Vincent and the Grenadines','VEN':'Venezuela, Bolivarian Republic of','VGB':'Virgin Islands, British','VIR':'Virgin Islands, U.S.','VNM':'Viet Nam','VUT':'Vanuatu','WLF':'Wallis and Futuna','WSM':'Samoa','YEM':'Yemen','ZAF':'South Africa','ZMB':'Zambia','ZWE':'Zimbabwe','URS':'Soviet Union','GDR':'East Germany','TCH':'Czechoslovakia'};
 
 	// Load the data file
 	loadJSON(getDataPath('#data'),parseIt,{error:function(){ $('.loader').html("Oops. Couldn't find the data."); }});
 
-	//parseTrips();
-
-	
 	// Add scroll event
 	$(document).on('scroll',function(e){ updateTimeline(getYFrac()); });
 
@@ -73,33 +71,31 @@ $(document).ready(function(){
 		html = '<ul class="timeline">';
 		for(var i = 0; i < trips.length; i++){
 			if((trips[i].launchday < d && trips[i].landday > d) || (trips[i].launchday < d && !trips[i].landday)){
-				html += '<li class="human '+trips[i].category+'"><div class="padder"><span class="name">'+trips[i].name+'</span></div><\/li>';
+				html += '<li class="human '+trips[i].category+'" id="'+i+'"><div class="padder"><span class="name">'+trips[i].name+'</span></div><\/li>';
 			}
 		}
 		html += '<\/ul>';
 
 		$('#inspace').html(html);
-	}
-	function parseTrips(){
-		var a = $('#inspace .timeline li');
-		for(var i = 0; i < a.length; i++){
-			var t = $(a[i]).find('time');
-			var launch = new Date($(t[0]).attr('datetime'));
-			var launchday = new Date($(t[0]).attr('datetime'));
-			if(launchday){
-				launchday.setHours(0);
-				launchday.setMinutes(0);
-				launchday.setSeconds(0);
+		// Set up the tooltip
+		tooltip({
+			'elements':$('.timeline .human'),
+			'html':function(){
+				var id = parseInt($(this).attr('id'));
+				var a = trips[id];
+				var text = '<div class="stripe '+a.category+'"><\/div><h3>'+a.name+'<\/h3><table>';
+				text += '<tr><td>Gender:<\/td><td>'+a.gender+'<\/td><\/tr>';
+				text += '<tr><td>Country:<\/td><td>'+formatArray(a.country,cc)+'<\/td><\/tr>';
+				text += '<tr><td>Year of birth:<\/td><td>'+a.dob.getFullYear()+'<\/td><\/tr>';
+				text += '<tr><td>Trips to space:<\/td><td>'+a.missions+'<\/td><\/tr>';
+				var start = a.launch.toISOString().substr(0,10);
+				var end = (a.land ? a.land.toISOString().substr(0,10) : '');
+				text += '<tr><td>This trip:<\/td><td>'+start+(end!=start ? ' - '+end : '')+'<br />('+formatArray(a.missionnames)+')<\/td><\/tr>';
+				text += '<\/table>';
+				text += '<a href="https://github.com/cosmos-book/cosmos-book.github.io/tree/master/human-spaceflight/data/'+a.file+'" class="repo">data file<\/a>';
+				return text;
 			}
-			var land = ($(t[1]).attr('datetime')!="" ? new Date($(t[1]).attr('datetime')) : '');
-			var landday = ($(t[1]).attr('datetime')!="" ? new Date($(t[1]).attr('datetime')) : '');
-			if(landday){
-				landday.setHours(23);
-				landday.setMinutes(59);
-				landday.setSeconds(59);
-			}
-			trips.push({'launch':launch,'land':land,'launchday':launchday,'landday':landday,'name':$(a[i]).find('.name').html(),'category':$(a[i]).attr('class').substr(6)});
-		}
+		});
 	}
 
 	function parseIt(data){
@@ -138,19 +134,20 @@ $(document).ready(function(){
 					landday.setMinutes(59);
 					landday.setSeconds(59);
 				}
-				trips.push({'launch':launch,'land':land,'launchday':launchday,'landday':landday,'name':name,'category':data[name].category});
-				//"ACTON, Loren W.":{"category":"astronaut","gender":"Male","dob":"1936-03-07","country":"USA","eva":0,"file":"ACTON_LW.md","missions":[{"names":"STS-51-F","a":"1985-07-29T21:00:00Z","b":"1985-08-06T19:45:27Z"}]},
-
-/*
-				a.name = name;
-				a.n = n;
-				a.width = dl;
-				a.left = l;
-				a.missionnames = data[name].missions[m].names.split(";");
-				a.launch = launch;
-				a.land = (data[name].missions[m].b ? land : "");
-				a.dob = new Date(a.dob);
-				astronauts.push(a);*/
+				trips.push({
+					'launch':launch,
+					'land':land,
+					'launchday':launchday,
+					'landday':landday,
+					'name':name,
+					'category':data[name].category,
+					'dob':new Date(data[name].dob),
+					'missions':data[name].missions.length,
+					'missionnames':data[name].missions[m].names.split(";"),
+					'country':data[name].country,
+					'gender':data[name].gender,
+					'file':data[name].file
+				});
 			}
 		}
 
@@ -159,6 +156,7 @@ $(document).ready(function(){
 	
 		updateTimeline(-1);
 		setFromAnchor();
+
 	}
 	// We provide a ";" separated list of values and format them into a nice, comma-separated string
 	// We can provide an optional "lookup" array to replace 
