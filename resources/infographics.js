6c0227e99554c316a9b47f7f4036224f94ad9b35
Stuart <gravatar@strudel.org.uk>
2015-09-12 12:09:11 +0100

    Improve toggles

--- resources/infographics.js
+++ resources/infographics.js
@@ -320,5 +320,85 @@ function getDataPath(el){
 	return url;
 }
 
+function Toggler(toggles){
+	this.toggles = (toggles && toggles.toggles ? toggles.toggles : {});
+	this.callback = (toggles && typeof toggles.click==="function" ? toggles.click : '');
+
+	return this;
+}
+
+Toggler.prototype.val = function(id){
+	return $('input[name='+id+']:checked').val();
+}
+
+// Build a toggle button
+// Inputs
+//   el       = jQuery element to add this to (it should have a length==1)
+//   id       = The unique ID to use
+//   toggle.a = { "value": "a", "id": "uniqueid_a", "checked": true, "label": "Left label" }
+//   toggle.b = { "value": "b", "id": "uniqueid_b", "checked": false, "label": "Right label" }
+Toggler.prototype.create = function(el,id,toggles){
+
+	if(!el || el.length !=1 || typeof el.append!=="function") return this;
+	if(!toggles || !toggles['on'] || !toggles['off']) return this;
+	if((typeof id!=="string") && (typeof toggles[0]!=="object") && (typeof toggles[1]!=="object")) return this;
+	// Remove any existing version of this toggle
+	el.find('toggler').remove();
+
+	if(!toggles['off'].id) toggles['off'].id = id+'_off';
+	if(!toggles['on'].id) toggles['on'].id = id+'_on';
+
+	this.toggles[id] = { 'html':'', 'states':{}};
+	this.toggles[id].states = toggles;
+
+	var lc = '<label class="toggle-label';
+	var html = "";
+	html = '<form><div class="toggleinput toggler'+(toggles['off'].checked ? '' : ' checked')+'">'+lc+'1" for="'+toggles['off'].id+'">'+(toggles['off'].label ? toggles['off'].label : '')+'</label>';
+	html += '<div class="toggle-bg">';
+	html += '	<input id="'+toggles['off'].id+'" type="radio" '+(toggles['off'].checked ? 'checked="checked" ' : '')+'name="'+id+'" value="off">';
+	html += '	<input id="'+toggles['on'].id+'" type="radio" '+(toggles['on'].checked ? 'checked="checked" ' : '')+'name="'+id+'" value="on">';
+	html += '	<span class="switch"></span>';
+	html += '</div>';
+	html += ''+lc+'2" for="'+toggles['on'].id+'">'+(toggles['on'].label ? toggles['on'].label : '')+'</label></div></form>';
+
+	this.toggles[id].html = html;
+
+	el.append(html);
+
+	var _obj = this;
+
+	el.on('click','.toggler',{id:id,me:this},function(e){
+		var input = $(this).find('input:checked');
+		if(input.attr('value')=="on"){
+			$(this).addClass('checked');
+			e.data.me.toggles[e.data.id].states['on'].checked = true;
+			e.data.me.toggles[e.data.id].states['off'].checked = false;
+		}else{
+			$(this).removeClass('checked');
+			e.data.me.toggles[e.data.id].states['on'].checked = false;
+			e.data.me.toggles[e.data.id].states['off'].checked = true;
+		}
+		if(typeof _obj.callback==="function") _obj.callback.call();
+	});
+
+	return this;
+}
+
+Toggler.prototype.toggle = function(id){
+	var t = this.toggles[id];
+	if(!t) return this;
+	if(t.states['off'].checked) $('#'+t.states['on'].id).trigger('click').closest('.toggler').trigger('click');
+	else $('#'+t.states['off'].id).trigger('click').closest('.toggler').trigger('click');
+	return this;
+}
+
+Toggler.prototype.change = function(id,v){
+	var t = this.toggles[id];
+	if(!t) return this;
+	if(v=='on' && $('#'+t.states['on'].id).length>0) $('#'+t.states['on'].id).trigger('click').closest('.toggler').trigger('click');
+	if(v=='off' && $('#'+t.states['off'].id).length>0) $('#'+t.states['off'].id).trigger('click').closest('.toggler').trigger('click');
+	return this;
+}
+
 // Make a log10 function if it doesn't exist
 if(!Math.log10 || typeof Math.log10!=="function"){ Math.log10 = function(v) { return Math.log(v)/2.302585092994046; }; }
--- we-are-stardust/wearestardust.js
+++ we-are-stardust/wearestardust.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	var table;
 	var categories = {};
-	var toggles = new Toggler();
+	var toggles = new Toggler({'click':updatePeriodicTable});
 	
 	function parseFile(d,attrs){
 		//He,Helium,Non-metal,18,1,Big Bang; Stars; Massive stars; Supernovae,
@@ -31,16 +31,16 @@ $(document).ready(function(){
 		}
 		
 		// Add toggles for each category
-		$('table.periodic').before('<ul class="key"></ul>');
+		$('table.periodic').before('<ul class="key"><li class="keyitem"><span class="keylabel">Formed in:</span></li></ul>');
 		for(c in categories){
 			$('.key').append('<li class="keyitem" id="'+c+'"><div class="keylabel">'+categories[c].name+'</div></li>');
-			toggles.addTo($('#'+c),c,{
+			toggles.create($('#'+c),c,{
 				"off": {"label":"off","checked": true }, 
 				"on": { "label":"on" }
 			});
 		}
 		$('.key').append('<li class="keyitem"><div class="keysymbol life"></div><div class="keylabel">Essential to life</div><form><div class="toggleinput toggler"></div></form></li>');
-		$('.key').append('<li class="keyitem"><div class="keysymbol formed"></div><div class="keylabel">Formed</div><form><div class="toggleinput toggler"></div></form></li>');
+	//	$('.key').append('<li class="keyitem"><div class="keysymbol formed"></div><div class="keylabel">Formed</div><form><div class="toggleinput toggler"></div></form></li>');
 		$(document).on('click','.keylabel',function(){ toggles.toggle($(this).parent().attr('id')) });
 
 
@@ -53,85 +53,6 @@ $(document).ready(function(){
 
 	// Load the files
 	loadCSV('data/element_origin.csv',parseFile,{});
-
-
-	function Toggler(toggles){
-		this.toggles = (toggles ? toggles : {});
-
-		return this;
-	}
-	
-	Toggler.prototype.val = function(id){
-		return $('input[name='+id+']:checked').val();
-	}
-
-	// Build a toggle button
-	// Inputs
-	//   el       = jQuery element to add this to (it should have a length==1)
-	//   id       = The unique ID to use
-	//   toggle.a = { "value": "a", "id": "uniqueid_a", "checked": true, "label": "Left label" }
-	//   toggle.b = { "value": "b", "id": "uniqueid_b", "checked": false, "label": "Right label" }
-	Toggler.prototype.addTo = function(el,id,toggles){
-
-		if(!el || el.length !=1 || typeof el.append!=="function") return this;
-		if(!toggles || !toggles['on'] || !toggles['off']) return this;
-		if((typeof id!=="string") && (typeof toggles[0]!=="object") && (typeof toggles[1]!=="object")) return this;
-		// Remove any existing version of this toggle
-		el.find('toggler').remove();
-
-		if(!toggles['off'].id) toggles['off'].id = id+'_off';
-		if(!toggles['on'].id) toggles['on'].id = id+'_on';
-
-		this.toggles[id] = { 'html':'', 'states':{}};
-		this.toggles[id].states = toggles;
-
-		var lc = '<label class="toggle-label';
-		var html = "";
-		html = '<form><div class="toggleinput toggler'+(toggles['off'].checked ? '' : ' checked')+'">'+lc+'1" for="'+toggles['off'].id+'">'+(toggles['off'].label ? toggles['off'].label : '')+'</label>';
-		html += '<div class="toggle-bg">';
-		html += '	<input id="'+toggles['off'].id+'" type="radio" '+(toggles['off'].checked ? 'checked="checked" ' : '')+'name="'+id+'" value="off">';
-		html += '	<input id="'+toggles['on'].id+'" type="radio" '+(toggles['on'].checked ? 'checked="checked" ' : '')+'name="'+id+'" value="on">';
-		html += '	<span class="switch"></span>';
-		html += '</div>';
-		html += ''+lc+'2" for="'+toggles['on'].id+'">'+(toggles['on'].label ? toggles['on'].label : '')+'</label></div></form>';
-
-		this.toggles[id].html = html;
-
-		el.append(html);
-
-		el.on('click','.toggler',{id:id,me:this},function(e){
-			var input = $(this).find('input:checked');
-			if(input.attr('value')=="on"){
-				$(this).addClass('checked');
-				e.data.me.toggles[e.data.id].states['on'].checked = true;
-				e.data.me.toggles[e.data.id].states['off'].checked = false;
-			}else{
-				$(this).removeClass('checked');
-				e.data.me.toggles[e.data.id].states['on'].checked = false;
-				e.data.me.toggles[e.data.id].states['off'].checked = true;
-			}
-			updatePeriodicTable();
-		});
-
-		return this;
-	}
-
-	Toggler.prototype.toggle = function(id){
-		var t = this.toggles[id];
-		if(!t) return this;
-		if(t.states['off'].checked) $('#'+t.states['on'].id).trigger('click').closest('.toggler').trigger('click');
-		else $('#'+t.states['off'].id).trigger('click').closest('.toggler').trigger('click');
-		return this;
-	}
-
-	Toggler.prototype.change = function(id,v){
-		var t = this.toggles[id];
-		if(!t) return this;
-		if(v=='on' && $('#'+t.states['on'].id).length>0) $('#'+t.states['on'].id).trigger('click').closest('.toggler').trigger('click');
-		if(v=='off' && $('#'+t.states['off'].id).length>0) $('#'+t.states['off'].id).trigger('click').closest('.toggler').trigger('click');
-		return this;
-	}
-
 
 	function updatePeriodicTable(){
 		var td,ok,i,j,c;
